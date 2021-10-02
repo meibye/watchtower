3e050d3c34d1b296e6079a76c8cc477a7ffc228e
meibye <michael@eibye.name>
2021-10-02 13:33:33 +0200

    Debug build-arm.sh

--- build-arm.sh
+++ build-arm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+echo "Before git describe"
+git describe --tags
+echo "After git describe"
 VERSION=$(git describe --tags)
 echo "Building $VERSION..."
 GOOS=linux GOARCH=arm GOARM=7 go build -o watchtower -ldflags "-X github.com/meibye/watchtower/internal/meta.Version=$VERSION"
